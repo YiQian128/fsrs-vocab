44306b54dbfaecd320876cf5559631ea69b42e39
YiQian128 <1668358980@qq.com>
2026-01-16 18:52:15 +0800

    feat: auth foundation (flyway + base api)

--- src/main/java/com/zkx/fsrsvocab/FsrsVocabApplication.java
+++ src/main/java/com/zkx/fsrsvocab/FsrsVocabApplication.java
@@ -1,8 +1,10 @@
 package com.zkx.fsrsvocab;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.zkx.fsrsvocab.modules.**.mapper")
 @SpringBootApplication
 public class FsrsVocabApplication {
 
